5d03d35dcd7d13be8293eefdae02cd8609e898f5
DulceTovar01 <duulcetovar@gmail.com>
2023-03-21 12:47:11 -0600

    vista registro rol

---
+++ src/views/RegistroRol.vue
@@ -0,0 +1,59 @@
+<script setup>
+</script>
+<template>
+    <body>
+        <div class ="container">
+            <div class ="row">
+
+                <div class ="col-md-2">
+                    
+                </div>
+                <div class ="col-md-6">
+                    <p class="fs-6" style="color:white" >Actualiizar Rol</p>
+                </div>
+                <!-- <div class ="col-md" >
+                    <p class="fs-6" style="color:white" > </p>
+                    <input class="form-control form-control-sm" type="text"  placeholder="🔍 Buscar">
+
+                </div> -->
+
+            </div>
+            <div class ="row">
+                <div class ="col-md-3">
+                    <p class="fs-6" style="color:white" >Nombre del Rol</p>
+                </div>
+                <div class ="col-md-3">
+                <button type="button" class="btn btn-success">Actualizar</button>
+                </div>
+                <div class ="col-md" >
+                    <p class="fs-2" style="color:white" > </p>
+                    <input class="form-control form-control-sm" type="text"  >
+
+                </div>
+
+            </div>
+            <div class ="row">
+                <div class ="col-md-3">
+                <p class="fs-6" style="color:white" >Permisos a los que tiene acceso:</p>
+            </div>
+        </div>
+    </div>
+        
+
+    </body>
+</template>
+<style>
+body{
+
+    background: linear-gradient(113.96deg, #000103 2.35%, #164193 100%);
+    font-family: 'Fjalla One', sans-serif;
+    font-family: 'Noto Sans Lao', sans-serif;
+}
+
+.fs-6{
+    font-family: 'Fjalla One', sans-serif;
+    font-size: 80px; /*puedes ajustar el tamaño de la fuente*/
+    font-weight: bold;
+}
+
+</style>
