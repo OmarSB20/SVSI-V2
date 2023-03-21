07e0aaa1d130dd4fef4fb80c9959ef7e2a2f58d9
Lugaldz <kink-2@hotmail.com>
2023-03-21 12:20:07 -0600

    interfaz crear roles

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-import CambiarRol from '../views/CambiarRol.vue'
+import CrearRol from '../views/CrearRol.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,7 +23,7 @@ const router = createRouter({
     {
       path: '/crearRol',
       name: 'crearRol',
-      component: CambiarRol
+      component: CrearRol
     }
   
   ]
